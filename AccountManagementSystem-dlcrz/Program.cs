20c64321368de1a2cfe25884e4a0474d06fc87f3
kurtpaolo <kurtpaoloredondo14@gmail.com>
2026-02-14 13:09:16 +0800

    FINAL

--- AccountManagementSystem-dlcrz/Program.cs
+++ AccountManagementSystem-dlcrz/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace AccountManagementSystem_dlcrz
+﻿namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("ACCOUNT MANAGEMENT SYSTEM");
+
+            string username = "superuser";
+            string password = "superuser123";
+
+            Console.WriteLine("Enter username: ");
+            string usernameinput = Console.ReadLine();
+            Console.WriteLine("Enter password: ");
+            string passwordinput = Console.ReadLine();
+
+
+
+            if (usernameinput == username && passwordinput == password)
+            {
+                Console.WriteLine("Log in Successful!!");
+            }
+            else
+            {
+                Console.WriteLine(" Wrong Credentials!!");
+            }
+
+
         }
     }
-}
